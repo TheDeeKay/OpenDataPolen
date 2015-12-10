1e7efe7f2f637674fe1a763ee24d75e0c9183273
Aleksa <aleksa1994@hotmail.com>
2015-12-10 23:32:16 +0100

    Fix plants list, strikethrough is now kept for deselected plants, FINALLY!

--- app/src/main/java/com/example/makina/polen/ListaBiljaka.java
+++ app/src/main/java/com/example/makina/polen/ListaBiljaka.java
@@ -1,9 +1,11 @@
 package com.example.makina.polen;
 
+import android.content.Context;
 import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -27,7 +29,7 @@ public class ListaBiljaka extends AppCompatActivity {
 
         List<String> listaBiljaka = new ArrayList<String>(Arrays.asList(biljke));
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+        NekiAdapter<String> adapter = new NekiAdapter<String>(this,
                 R.layout.lista_biljaka_item,
                 R.id.fragment_list_item_textview,
                 listaBiljaka);
@@ -36,7 +38,7 @@ public class ListaBiljaka extends AppCompatActivity {
 
         list.setAdapter(adapter);
 
-        }
+    }
 
     //onClick za item iz liste biljaka, precrtava i oznacava da se ne gledaju
     public void onClick(View view) {
@@ -59,6 +61,25 @@ public class ListaBiljaka extends AppCompatActivity {
 
     }
 
+    //Novi adapter, samo da bi se zadrzavao strikethrough prilikom skrolovanja i ponovnog otvaranja liste
+    private class NekiAdapter<String> extends ArrayAdapter<String> {
+
+        public NekiAdapter(Context context, int resource, int textViewResourceId, List<String> objects) {
+            super(context, resource, textViewResourceId, objects);
+        }
+
+        @Override
+        public TextView getView(int position, View convertView, ViewGroup parent) {
+            View view = super.getView(position, convertView, parent);
+            TextView text = (TextView) view;
+            if (MainActivity.biljkeChecked[position] == 1)
+                text.setPaintFlags(text.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+            else text.setPaintFlags(text.getPaintFlags() & ~Paint.STRIKE_THRU_TEXT_FLAG);
+            return text;
+
+        }
+
+    }
 }
 
 
